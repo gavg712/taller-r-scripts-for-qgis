60de82dad9e581b0f75241891a1ec802cc2f5a51
gabo <gavg712@gmail.com>
2022-04-20 12:29:49 -0500

    Adding new demo script

---
+++ collections/user2022-rscripts/rscripts/demo_zonal_mean_top_n.R
@@ -0,0 +1,20 @@
+##Raster processing=group
+##zonalmeantopn=name
+##Zonal mean from top N=display_name
+##Raster=raster
+##Zonas=vector polygon
+##Zonas_Ids=Field Zonas
+##Zonal=output vector
+
+Zonas_rst <- raster::rasterize(Zonas, Raster, field = Zonas_Ids)
+mean_top_100 <- function(x, top = 100, na.rm = TRUE) {
+  if(na.rm) x <- na.omit(x)
+  if(length(x) < top) {
+    top <- length(x)
+    message("Zone has less than top n values defined. Calculating with availables")
+  }
+  mean(x[which(rank(-x) <= top)][seq_len(top)])
+}
+Stats <- raster::zonal(Raster, Zonas_rst, fun = mean_top_100)
+names(Stats) <- c(Zonas_Ids, "mean")
+Zonal <- merge(Zonas, Stats, by.x = "id", by.y = "zone")
